b7ee5763bc53212026b96690371679fe8bc9c06b
charoandina <rosario.garciadea@gmail.com>
2023-11-14 12:43:05 +0100

    JS

--- main.js
+++ main.js
@@ -2,12 +2,51 @@
 //VARIABLES DOM
 let figmaIcon = document.getElementById("figmaIcon");
 let jsIcon = document.getElementById("jsIcon");
+let HTMLIcon = document.getElementById("HTMLIcon");
+let gitIcon = document.getElementById("gitIcon");
+let cssIcon = document.getElementById("cssIcon");
 
-function changeImage() {
+function changeImageFigma() {
     figmaIcon.src = "img/figma2.png";
 }
 
-function backToImage() {
+function backToImageFigma() {
     figmaIcon.src = "img/figma.png";
+}
+
+
+function changeImageJS() {
+    jsIcon.src = "img/javascript2.png";
+}
+
+function backToImageJS() {
+    jsIcon.src = "img/javascript.png";
+}
+
+
+function changeImageHTML() {
+    HTMLIcon.src = "img/html2.png";
+}
+
+function backToImageHTML() {
+    HTMLIcon.src = "img/html.png";
+}
+
+
+function changeImageGit() {
+    gitIcon.src = "img/git2.png";
+}
+
+function backToImageGit() {
+    gitIcon.src = "img/git.png";
+}
+
+
+function changeImageCSS() {
+    cssIcon.src = "img/css2.png";
+}
+
+function backToImageCSS() {
+    cssIcon.src = "img/css3.png";
 }
 
