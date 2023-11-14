33f3dd4444b85518a2b5cca735d9f4882434678e
charoandina <rosario.garciadea@gmail.com>
2023-11-14 11:37:33 +0100

    modificaciones 14-11

---
+++ main.js
@@ -0,0 +1,13 @@
+// FUNCTIONS - STACK
+//VARIABLES DOM
+let figmaIcon = document.getElementById("figmaIcon");
+let jsIcon = document.getElementById("jsIcon");
+
+function changeImage() {
+    figmaIcon.src = "img/figma2.png";
+}
+
+function backToImage() {
+    figmaIcon.src = "img/figma.png";
+}
+
